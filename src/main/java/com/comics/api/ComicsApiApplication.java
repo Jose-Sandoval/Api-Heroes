41e21f3ed7ebd13f8c1c6dcd83c3bfe8242e154e
Jose-S <mrquad47@gmail.com>
2020-12-07 19:02:44 -0600

    cambiando la URL local

--- src/main/java/com/comics/api/ComicsApiApplication.java
+++ src/main/java/com/comics/api/ComicsApiApplication.java
@@ -18,7 +18,7 @@ public class ComicsApiApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**").allowedOrigins("http://localhost:8080","http://localhost:5500/").allowedMethods("*");
+				registry.addMapping("/api/**").allowedOrigins("http://127.0.0.1:5500").allowedMethods("*");
 			}
 		};
 	}

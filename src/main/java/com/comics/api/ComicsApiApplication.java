c5ef415c1fd0261cb369580715d732f08d4c336e
Jose-S <mrquad47@gmail.com>
2020-12-07 18:31:27 -0600

    activando CORS

--- src/main/java/com/comics/api/ComicsApiApplication.java
+++ src/main/java/com/comics/api/ComicsApiApplication.java
@@ -2,6 +2,9 @@ package com.comics.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ComicsApiApplication {
@@ -9,5 +12,16 @@ public class ComicsApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ComicsApiApplication.class, args);
 	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("http://localhost:8080","http://localhost:5500/").allowedMethods("*");
+			}
+		};
+	}
+
 
 }
